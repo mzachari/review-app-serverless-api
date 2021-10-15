41197793b477888b62965b17e8ea26cc143e0327
mzachari <mariazacharia.k@gmail.com>
2021-10-15 23:41:32 +0530

    added add product review API

--- services/reviews/reviewController.js
+++ services/reviews/reviewController.js
@@ -13,3 +13,15 @@ export async function getProductReviews(event, context){
         return failure("Internal Server Error");
       }
 }
+
+export async function addProductReview(event, context){
+  const productId = event.pathParameters.productId;
+  const {rating, reviewText} = JSON.parse(event.body);
+  try {
+      const data = await reviewService.addProductReview(productId, rating, reviewText);
+      return success(data);
+    } catch (error) {
+      console.log("Failed to fetch product reviews: ", error);
+      return failure("Internal Server Error");
+    }
+}
--- services/reviews/reviewService.js
+++ services/reviews/reviewService.js
@@ -1,4 +1,5 @@
 import dynamoDb from "../../libs/dynamodb-lib";
+import * as uuid from "uuid";
 
 export default {
   getProductReviews: (productId) => {
@@ -15,6 +16,26 @@ export default {
         const getProductReviewsResult = await dynamoDb.query(getProductReviewsParams);
         const productReviews = getProductReviewsResult.Items || {};
         resolve(productReviews);
+      }catch(error){
+          reject(error);
+      }
+    });
+  },
+  addProductReview: (productId, rating, reviewText) => {
+    return new Promise(async (resolve, reject) => {
+      const addProductReviewParams = {
+        TableName: process.env.reviewTable,
+        Item: {
+          productId: productId,
+          reviewId: uuid.v4(),
+          reviewText: reviewText,
+          rating: rating
+        },
+      };
+      try{
+        await dynamoDb.put(addProductReviewParams);
+        const productReview = addProductReviewParams.Item;
+        resolve(productReview);
       }catch(error){
           reject(error);
       }
